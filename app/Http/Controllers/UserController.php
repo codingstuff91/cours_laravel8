ae5c88b8961684ecfd50138694a0adb8a0b5221a
Matthieu MARTIN <codingstuff91@gmail.com>
2021-07-09 08:39:32 +0200

    Ajout vue creation et ajout logique creation user dans controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,27 +2,106 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return 'Liste de tous les utilisateurs';
+        return User::all();
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
-        return 'futur contenu de ma vue de création';
+        $roles = Role::all();
+        return view('user.create')->with('roles',$roles);
     }
 
-    public function show()
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        return 'Utilisateur précis retourné';
+        // Validation de l'objet Request envoyé par le formulaire
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+
+        // Requete création utilisateur
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // Affectation d'un role pour cet utilisateur
+        $user->roles()->attach($request->role);
+
+        return redirect('user');
     }
 
-    public function find()
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
     {
-        return 'Utilisateur retrouvé par son nom';
+        return $user;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
     }
 }
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,22 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <h1>Création d'un nouvel utilisateur</h1>
 
-    <form action="{{ route('user.store') }}">
+    <form action="{{ route('user.store') }}" method="POST">
+        @csrf
+
         <label for="">Pseudo</label>
         <input type="text" name="name">
         
         <label>Email</label>
-        <input type="text" name="name">
+        <input type="text" name="email">
         
         <label>Mot de passe</label>
         <input type="text" name="password">
 
         <label>Role accordé</label>
-        <select name="role">
+        <select name="role[]" multiple>
             @foreach ($roles as $role)
-                <option value="{{$role->name}}">{{$role->name}}</option>
+                <option value="{{ $role->id }}">{{ $role->name }}</option>
             @endforeach
         </select>
 
